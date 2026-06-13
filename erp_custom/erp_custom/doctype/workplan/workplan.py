0f47a6e6f8962534c5a03cc31d01d531af0db8d7
DAPL-PROD-1986 <karthik.gowrishankar@dynatherm.co.in>
2026-06-13 11:55:39 +0530

    Workplan, Expense Claim

--- erp_custom/erp_custom/doctype/workplan/workplan.py
+++ erp_custom/erp_custom/doctype/workplan/workplan.py
@@ -8,13 +8,16 @@ class Workplan(Document):
 
     def on_submit(self):
         frappe.sendmail(
-            recipients=["DAPL-team@dynatherm.co.in"],
+			# recipients=["msk312508@gmail.com"],
+            # sender="karthickarjunan08@gmail.com",
+
+			recipients=["DAPL-team@dynatherm.co.in"],
             sender="production@dynatherm.co.in",
             subject=f"Today's {self.doctype} Submitted - {self.name}",
             message=f"""
                 <p> Dear Team, </p>
 				<p> Today's Workplan <b>{self.name}</b> has been submitted successfully for production activities. </p>
-				<p><a href="{{ frappe.utils.get_url_to_form(self.doctype, self.name) }}"
+				<p><a href="{frappe.utils.get_url_to_form(self.doctype, self.name)}"
 					style="background-color:#28a745;
 							color:white; padding:10px 16px;
 							text-decoration:none; border-radius:5px;
